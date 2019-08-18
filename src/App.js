fd244c4c984d7233c2e9916001e4fc8237226958
Kiet <kietlo@gmail.com>
2019-08-18 22:11:28 +0100

    Conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./index.css";
 import { getMovies } from "./services/fakeMovieService";
 
 class App extends Component {
@@ -13,33 +13,40 @@ class App extends Component {
   };
 
   render() {
+    if (this.state.movies.length === 0) {
+      return <span>No movies left</span>;
+    }
+
     return (
-      <main class="container">
-        <table>
-          <thead>
-            <tr>
-              <th>Tile</th>
-              <th>Genre</th>
-              <th>Stock</th>
-              <th>Rate</th>
-              <th />
-            </tr>
-          </thead>
-          <tbody>
-            {this.state.movies.map(m => (
-              <tr key={m._id}>
-                <td>{m.title}</td>
-                <td>{m.genre.name}</td>
-                <td>{m.numberInStock}</td>
-                <td>{m.dailyRentalRate}</td>
-                <td>
-                  <button onClick={() => this.handleDelete(m)}>Delete</button>
-                </td>
+      <React.Fragment>
+        <span>{"Number of movies left: " + this.state.movies.length}</span>;
+        <main class="container">
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Tile</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th />
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </main>
+            </thead>
+            <tbody>
+              {this.state.movies.map(m => (
+                <tr key={m._id}>
+                  <td>{m.title}</td>
+                  <td>{m.genre.name}</td>
+                  <td>{m.numberInStock}</td>
+                  <td>{m.dailyRentalRate}</td>
+                  <td>
+                    <button onClick={() => this.handleDelete(m)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </main>
+      </React.Fragment>
     );
   }
 }
