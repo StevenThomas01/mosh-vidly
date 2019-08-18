4077fcbc8fcddea02b755e562ab5b2cbe07c607a
Kiet <kietlo@gmail.com>
2019-08-18 12:32:38 +0100

    Delete of records

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { getMovies } from "./services/fakeMovieService";
 
-function App() {
-  return (
-    <main class="container">
-      <h1>Hello Steven</h1>
-    </main>
-  );
+class App extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  };
+
+  render() {
+    return (
+      <main class="container">
+        <table>
+          <thead>
+            <tr>
+              <th>Tile</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th />
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(m => (
+              <tr key={m._id}>
+                <td>{m.title}</td>
+                <td>{m.genre.name}</td>
+                <td>{m.numberInStock}</td>
+                <td>{m.dailyRentalRate}</td>
+                <td>
+                  <button onClick={() => this.handleDelete(m)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
+    );
+  }
 }
 
 export default App;
